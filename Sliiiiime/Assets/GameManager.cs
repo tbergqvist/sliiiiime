efcb03ade217d7128d7d43156f56de07d413cc33
DavidBjorkberg <david.bjorkberg1@gmail.com>
2021-11-05 23:38:33 +0100

    Added victory screen

---
+++ Sliiiiime/Assets/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public enum PlayerNumber { Player1, Player2, Player3 };
+
+    public static GameManager Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public void PlayerDied()
+    {
+        Slime[] allPlayers = FindObjectsOfType<Slime>();
+        int nrOfAlivePlayers = 0;
+        foreach (Slime player in allPlayers)
+        {
+            if(player.isAlive)
+            {
+                nrOfAlivePlayers++;
+            }
+        }
+        if(nrOfAlivePlayers == 1)
+        {
+            foreach (Slime player in allPlayers)
+            {
+                if (player.isAlive)
+                {
+                    EndGame(player.playerNumber);
+                }
+            }
+        }
+    }
+    public void EndGame(PlayerNumber winner)
+    {
+        GameObject UI = GameObject.Find("UI");
+        Text winText = UI.transform.Find("WinText").GetComponent<Text>();
+        switch (winner)
+        {
+            case PlayerNumber.Player1:
+                winText.color = Color.red;
+                winText.text = "Red WON!";
+                break;
+            case PlayerNumber.Player2:
+                winText.color = Color.green;
+                winText.text = "Green WON!";
+                break;
+            case PlayerNumber.Player3:
+                winText.color = Color.blue;
+                winText.text = "Blue WON!";
+                break;
+            default:
+                break;
+        }
+    }
+}
