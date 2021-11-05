d5dbcd55705d123e418857a84fb540588f842910
DavidBjorkberg <david.bjorkberg1@gmail.com>
2021-11-05 19:17:27 +0100

    Player doesnt glitch into the wall if he walks into it

--- Sliiiiime/Assets/Movement.cs
+++ Sliiiiime/Assets/Movement.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public string inputAxisSuffix; 
+    public string inputAxisSuffix;
     public float speed = 100f;
     public float jumpHeight = 250f;
+    public float wallCheckRange;
 
     bool isJumping = false;
     Rigidbody2D rb;
@@ -20,7 +21,11 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position += new Vector3(speed, 0) * Time.deltaTime * Input.GetAxis("Horizontal" + inputAxisSuffix);
+        float horizontalMove = Input.GetAxis("Horizontal" + inputAxisSuffix);
+        if (horizontalMove != 0)
+        {
+            TryMove(horizontalMove);
+        }
         if (Input.GetAxis("Vertical" + inputAxisSuffix) > 0.8f && !isJumping)
         {
             Jump();
@@ -39,4 +44,26 @@ public class Movement : MonoBehaviour
         rb.AddForce(new Vector2(0, jumpHeight));
         isJumping = true;
     }
+    void TryMove(float horizontalMove)
+    {
+        Vector3 moveVector = new Vector3(speed, 0) * Time.deltaTime * horizontalMove;
+        if (!WillWalkIntoWall(moveVector.normalized))
+        {
+            gameObject.transform.position += moveVector;
+        }
+    }
+    bool WillWalkIntoWall(Vector3 moveVector)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, moveVector, wallCheckRange * transform.localScale.x);
+        if(hit)
+        {
+            print(hit.transform.name);
+            if (hit.transform.TryGetComponent<Platform>(out _))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
