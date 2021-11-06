71723b62275c8aedf516c5be8fa11c8b5b94d88e
Alen Gazibegovic <alen.gazibegovic@piktiv.se>
2021-11-06 10:25:45 +0100

    Fix projectile colors

--- Sliiiiime/Assets/EnemyLogic.cs
+++ Sliiiiime/Assets/EnemyLogic.cs
@@ -90,11 +90,11 @@ public class EnemyLogic : MonoBehaviour
 
         var spawnedProjectile = Instantiate(projectile, new Vector3(withOffset.x, withOffset.y, 0), Quaternion.Euler(direction));
         spawnedProjectile.transform.Rotate(0, 0, bla / Mathf.PI * 180 + 90);
-        spawnedProjectile.transform.localScale *= 0.5f;
+        spawnedProjectile.transform.localScale = gameObject.transform.localScale * (0.1f);
         spawnedProjectile.GetComponent<Rigidbody2D>().AddForce(projectileForce, ForceMode2D.Impulse);
         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), spawnedProjectile.GetComponent<Collider2D>());
         spawnedProjectile.GetComponent<Projectile>().playerOwner = this.gameObject;
-
+        spawnedProjectile.GetComponent<Projectile>().GetComponentInChildren<MeshRenderer>().material.color = Color.green;
         Destroy(spawnedProjectile, 10);
         fireTime += Time.time + fireRepeatTime;
     }
