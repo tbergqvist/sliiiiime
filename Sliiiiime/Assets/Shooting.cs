ffd201cd149ab8d2bcd64fbd07988d4feab42111
DavidBjorkberg <david.bjorkberg1@gmail.com>
2021-11-06 01:33:54 +0100

    Controller now aims with right joystick

--- Sliiiiime/Assets/Shooting.cs
+++ Sliiiiime/Assets/Shooting.cs
@@ -13,14 +13,14 @@ public class Shooting : MonoBehaviour
 
     void Start()
     {
-       
+
     }
 
     void Update()
     {
         shootTimer -= Time.deltaTime;
 
-        if(Input.GetAxis("Fire" + inputAxisSuffix) > 0.8f && shootTimer <= 0)
+        if (Input.GetAxis("Fire" + inputAxisSuffix) > 0.8f && shootTimer <= 0)
         {
             ShootProjectile();
             shootTimer = shootCooldown;
@@ -29,19 +29,35 @@ public class Shooting : MonoBehaviour
     void ShootProjectile()
     {
         GameManager.Instance.PlaySound(shootSound);
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = (mousePosition - new Vector2(transform.position.x, transform.position.y)).normalized;
+        Vector2 direction = GetShootDirection();
         Vector3 projectileForce = direction * projectileSpeed;
         var bla = Mathf.Atan2(direction.y, direction.x);
         var withOffset = new Vector2(transform.position.x, transform.position.y) + direction * 0.5f;
 
         var spawnedProjectile = Instantiate(projectile, new Vector3(withOffset.x, withOffset.y, 0), Quaternion.Euler(direction));
         spawnedProjectile.transform.Rotate(0, 0, bla / Mathf.PI * 180 + 90);
-        spawnedProjectile.transform.localScale = gameObject.transform.localScale *(0.3f);
+        spawnedProjectile.transform.localScale = gameObject.transform.localScale * (0.3f);
         spawnedProjectile.GetComponent<Rigidbody2D>().AddForce(projectileForce, ForceMode2D.Impulse);
         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), spawnedProjectile.GetComponent<Collider2D>());
         spawnedProjectile.GetComponent<Projectile>().playerOwner = gameObject;
 
         Destroy(spawnedProjectile, 10);
+    }
+    Vector2 GetShootDirection()
+    {
+        if (GetComponent<Slime>().playerNumber == GameManager.PlayerNumber.Player1)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (mousePosition - new Vector2(transform.position.x, transform.position.y)).normalized;
+            return direction;
+        }
+        else
+        {
+            float horizontalAxis = Input.GetAxis("HorizontalAim" + inputAxisSuffix);
+            float verticalAxis = Input.GetAxis("VerticalAim" + inputAxisSuffix);
+            Vector2 direction = new Vector2(horizontalAxis, verticalAxis).normalized;
+            return direction; 
+        }
+        
     }
 }
